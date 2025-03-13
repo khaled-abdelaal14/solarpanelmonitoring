74ff76c6995fd5740cfe6219cf3779e261f4534e
khaled-abdelaal14 <khaledabdelaala2@gmail.com>
2025-03-13 23:16:58 +0200

    committ

--- app/Http/Controllers/Api/DeviceController.php
+++ app/Http/Controllers/Api/DeviceController.php
@@ -79,7 +79,7 @@ class DeviceController extends Controller
             'sensortype' => 'required',
             'sensorvalue' => 'required|numeric',
             'battery_serial_number' => 'required|unique:batteries,serial_number',
-            //22
+            //222
             'sensorname' => 'required',
             'sensortype' => 'required',
             'sensorvalue' => 'required|numeric',
